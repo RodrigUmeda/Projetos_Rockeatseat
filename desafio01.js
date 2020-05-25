29d3c334939373a5775a5ac204fbaac99b546f3f
Rodrigo <rumeda21@hotmail.com>
2020-05-25 18:10:43 -0300

    Desafio01

---
+++ desafio01.js
@@ -0,0 +1,43 @@
+//const nome = 'Rodrigo'
+//const peso = 61
+//const altura = 1.70
+
+//const IMC = (peso / (altura^2))
+
+//if (IMC >=30){
+ //   console.log('Rodrigo esta acima do peso');
+//}else{
+//    console.log('Rodrigo não esta acima do peso');
+//}
+
+//APOSENTADORIA
+
+// const nome = 'Ossamu'
+// const idade = 68
+// const sexo = 'M'
+// const TempoContribuiçao = 35
+// const TempoMinimoHomem = 35
+// const TempoMinimoMulher = 30
+// const VerificacaoAposentarHomem = sexo === 'M' && TempoContribuiçao >= TempoMinimoHomem && (idade + TempoContribuiçao >= 95) 
+// const VerificacaoAposentarMulher = sexo == 'F' && TempoContribuiçao >= TempoMinimoMulher && (idade + TempoContribuiçao >=85)
+
+
+
+// if (VerificacaoAposentarHomem || VerificacaoAposentarMulher){
+//     console.log(` ${nome} Ossamu, você pode se aposentar`);
+// }else{
+// console.log(` ${nome}, você não pode se aposentar`);
+// }
+
+
+
+ 
+    
+        
+
+
+
+
+
+
+
